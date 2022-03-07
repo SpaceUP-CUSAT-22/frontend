1b962c6490af92053b85214edd62d6fd0e8ecd49
Vishnu S <63354441+VishnuS-2000@users.noreply.github.com>
2022-03-07 20:33:10 +0530

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,51 @@
+import React from "react"
+import "./home.css"
+
+export default function Home(){
+
+
+
+    return <div className="wrapper">
+
+    <div className="home__left">
+    
+
+    <img src="./space_up.svg" className="home-logo"/>
+
+    <div className="home__text">
+  
+    <h1 className="home-title">Lorem Ipsum is 
+    simply dummy text</h1>
+
+
+    <p className="home-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text
+    Lorem Ipsum is simply dummy text of the printing </p>
+    
+
+  
+
+
+ 
+
+
+    <button className="home-button">Register Now</button>
+    
+    </div>
+
+    </div>
+    
+
+    <div className="home__right">
+    
+    
+    </div>
+    
+    
+    </div>
+
+
+}
+
+
+
+
